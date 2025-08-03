cb4e4163d23abf3c288a893c2dbfd33b54746ae1
Awais292306 <awaiskhn0090@gmail.com>
2025-08-03 10:43:58 -0700

    uploading first assignment

---
+++ assignment 1.py
@@ -0,0 +1,46 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+========== RESTART: C:/Users/SKYTECH COMPUTERS/Desktop/ASSINGMENT 1.PY =========
+ Part.1: = Arthmetic operators 
+Addition: a+b = 20
+Subtraction: a-b = 10
+Multiplications: a*b = 75
+division: a/b = 3.0
+Floor division: a//b = 3
+Modulus: a%b = 0
+Exponentiation: a**b = 759375
+ Q.1 = What do  you expect for floor division and modulus?
+ Part.2: = Arthmetic Assignment operators 
+Initial value of x; 10
+After x += 5: 15
+After x *= 2: 30
+After x -= 4: 26
+After x /= 2: 13.0
+ Q.2 = What will be the final value of x?
+ Part.3: = Comparison operators 
+a == b: False
+a! = b: True
+a > b: False
+a < b: True
+a >= b: False
+a <= b: True
+ Q.3 = Which comparisons return true?
+ Part.4: = Logical operators 
+x and y: False
+x or y: True
+not x: False
+ Q.4 = Predict the result before running each line?
+ Part.5: = Membership operators 
+'s' in institute: True
+'Saylani' in institute: False
+'Saylani' not in institute: True
+ Part.6 = Identity operators 
+a is b: False
+a is c: False
+c is not b: True
+ Bonus challenge 
+Enter your username: 
+Enter your password: 
+invalid username or password.
+>>> 
